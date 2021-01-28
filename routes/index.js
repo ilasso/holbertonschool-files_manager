0d3d1791247e40ce9724c3a5184b9fe12f74f525
ilasso <1204@holbertonschool.com>
2021-01-28 14:37:23 -0500

    Create a new user

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
 
@@ -15,6 +16,10 @@ const routeController = (app) => {
 
   router.get('/stats', (req, res) => {
     AppController.getStats(req, res);
+  });
+
+  router.post('/users', (req, res) => {
+    UsersController.postNew(req, res);
   });
 };
 
